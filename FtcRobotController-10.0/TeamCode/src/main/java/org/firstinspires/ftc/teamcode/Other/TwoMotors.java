99266fc4c164be92074ecc2db0d2734b28b3fc3c
Jokpau <122372967+Jokpau@users.noreply.github.com>
2025-11-04 18:18:09 +0200

    Fix: Fix another out of bounds error in motor speed control

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TwoMotors.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TwoMotors.java
@@ -71,7 +71,7 @@ public class TwoMotors extends LinearOpMode {
             if(edgeDetection.rising(GamepadIndexValues.dpadUp) && currentStep < 5)
                 currentStep ++;
 
-            if(edgeDetection.rising(GamepadIndexValues.dpadDown) && currentStep >= 0)
+            if(edgeDetection.rising(GamepadIndexValues.dpadDown) && currentStep > 0)
                 currentStep --;
 
             if(edgeDetection.rising(GamepadIndexValues.square))
